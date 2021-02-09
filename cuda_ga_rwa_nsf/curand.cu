06afb30c89331d58fb4b3e2546c348f082e1b505
marcosavedra <66279705+marcosavedra@users.noreply.github.com>
2021-02-09 16:57:16 -0300

    Add files via upload

---
+++ cuda_ga_rwa_nsf/curand.cu
@@ -0,0 +1,16 @@
+#include <curand_kernel.h>
+#include <cuda_runtime.h>
+#include "util.cuh"
+
+__device__ curandStateXORWOW_t state;
+
+__device__ double dRand(int max)
+{
+    //curandState state;
+    //curandStateXORWOW_t state;
+    int tId = (blockDim.x * blockIdx.x) + threadIdx.x;
+    curand_init((unsigned long long)clock() + tId, 0, 0, &state);
+
+    return max * curand_uniform(&state);
+}
+
